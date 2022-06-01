8f8e32cbe0b7ba079f42e1fae801f3a5e774091d
Getacher <Getacher.d@gmail.com>
2022-06-01 21:14:12 +0300

    print bin

--- 3-util.c
+++ 3-util.c
@@ -81,3 +81,51 @@ int print_ptr(va_list ls)
 	_putchar('x');
 	return (2 + print_p(n));
 }
+/**
+ * print_b - print binary
+ * @n: num to print
+ * Return: num char printed
+ */
+int print_b(unsigned int n)
+{
+	int i  = 0;
+	if ((n / 2) == 0)
+	{
+		_putchar((n % 2) + '0');
+		return (1);
+	}
+	else
+	{
+		_putchar((n % 2) + '0');
+		i = print_b(n / 2);
+		return (i  + 1);
+	}
+}
+/**
+ * print_bin - print binary
+ * @ls: args list
+ * Return: num char printed
+ */
+int print_bin(va_list ls)
+{
+	unsigned int n = va_arg(ls, unsigned int);
+
+	return (print_b(n));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
